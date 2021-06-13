23697d27f958b4a2e88265de32d69b555cd90555
Kuba Szczodrzyński <kuba@szczodrzynski.pl>
2021-06-13 18:33:20 +0200

    Add MQTT config parameters

--- WakeOnESP.ino
+++ WakeOnESP.ino
@@ -7,16 +7,13 @@
 WiFiManager wm;
 WiFiManagerParameter username("username", "HTTP Username", NULL, 16);
 WiFiManagerParameter password("password", "HTTP Password", NULL, 32);
+WiFiManagerParameter mqtt_broker("mqtt_broker", "MQTT Broker Address", NULL, 48);
+WiFiManagerParameter mqtt_username("mqtt_username", "MQTT Username", NULL, 16);
+WiFiManagerParameter mqtt_password("mqtt_password", "MQTT Password", NULL, 32);
+WiFiManagerParameter mqtt_ip("mqtt_ip", "MQTT VPN IP", NULL, 15);
+WiFiManagerParameter mqtt_secret("mqtt_secret", "MQTT Encryption Password", NULL, 16);
 
 char config_file[] = "/config.json";
-
-void printConfig()
-{
-    Serial.print("HTTP username = ");
-    Serial.println(username.getValue());
-    Serial.print("HTTP password = ");
-    Serial.println(password.getValue());
-}
 
 void readConfig()
 {
@@ -28,6 +25,8 @@ void readConfig()
             size_t size = file.size();
             std::unique_ptr<char[]> buf(new char[size]);
             file.readBytes(buf.get(), size);
+            Serial.println(buf.get());
+
             DynamicJsonDocument json(1024);
             DeserializationError error = deserializeJson(json, buf.get());
             if (error) {
@@ -42,8 +41,16 @@ void readConfig()
             username.setValue(tmp, strlen(tmp));
             tmp = json["password"];
             password.setValue(tmp, strlen(tmp));
-
-            printConfig();
+            tmp = json["mqtt_broker"];
+            mqtt_broker.setValue(tmp, strlen(tmp));
+            tmp = json["mqtt_username"];
+            mqtt_username.setValue(tmp, strlen(tmp));
+            tmp = json["mqtt_password"];
+            mqtt_password.setValue(tmp, strlen(tmp));
+            tmp = json["mqtt_ip"];
+            mqtt_ip.setValue(tmp, strlen(tmp));
+            tmp = json["mqtt_secret"];
+            mqtt_secret.setValue(tmp, strlen(tmp));
         }
 
         file.close();
@@ -60,6 +67,11 @@ void saveConfig()
     DynamicJsonDocument json(1024);
     json["username"] = username.getValue();
     json["password"] = password.getValue();
+    json["mqtt_broker"] = mqtt_broker.getValue();
+    json["mqtt_username"] = mqtt_username.getValue();
+    json["mqtt_password"] = mqtt_password.getValue();
+    json["mqtt_ip"] = mqtt_ip.getValue();
+    json["mqtt_secret"] = mqtt_secret.getValue();
 
     File file = SPIFFS.open(config_file, "w");
     if (!file) {
@@ -113,6 +125,11 @@ void setup()
 
     wm.addParameter(&username);
     wm.addParameter(&password);
+    wm.addParameter(&mqtt_broker);
+    wm.addParameter(&mqtt_username);
+    wm.addParameter(&mqtt_password);
+    wm.addParameter(&mqtt_ip);
+    wm.addParameter(&mqtt_secret);
     wm.setSaveParamsCallback(saveConfig);
 
     Serial.println("Connecting...");
@@ -131,10 +148,36 @@ void setup()
     Serial.println(WiFi.localIP());
 
     wm.setWebServerCallback(setupAuth);
+    wm.setParamsPage(true);
+    wm.setCustomHeadElement("<script>fetch('/mqttinfo').then((r) => r.text()).then((t) => document.getElementsByClassName('wrap')[0].innerHTML += t);</script>");
     wm.startWebPortal();
 
     wm.server->on("/hello", []() {
         wm.server->send(200, "text/plain", "Hello world");
+    });
+
+    wm.server->on("/mqttinfo", []() {
+        wm.server->setContentLength(CONTENT_LENGTH_UNKNOWN);
+        wm.server->send(200, "text/html", "<div class='msg ");
+        if (strlen(mqtt_broker.getValue()) == 0) {
+            wm.server->sendContent("P'>MQTT Disabled</div>");
+            return;
+        }
+
+        bool mqttConnected = true;
+        if (mqttConnected) {
+            wm.server->sendContent("S'><strong>MQTT Connected: </strong>");
+        }
+        else {
+            wm.server->sendContent("D'><strong>MQTT Disconnected: </strong>");
+        }
+
+        wm.server->sendContent(mqtt_broker.getValue());
+        wm.server->sendContent("<br/><em><small>Node IP: ");
+        wm.server->sendContent(mqtt_ip.getValue());
+        wm.server->sendContent("</small></em></div>");
+
+        wm.server->sendContent("");
     });
 }
 
